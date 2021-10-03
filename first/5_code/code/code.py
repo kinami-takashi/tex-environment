f8f7cccea295180fd8f643c71d770346ea13034f
kinami-takashi <er17026-6673@sti.chubu.ac.jp>
2021-10-04 03:11:52 +0900

    add

---
+++ first/5_code/code/code.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# 行列Aの定義
+matA = np.array([[2.0, 5.0, 8.0],
+                 [1.0, 3.0, 6.0],
+                 [7.0, 9.0, 4.0]])
+
+# 行列Aのスカラー倍の計算
+matAns = matA * 3
+
+# 計算結果の表示
+print(matAns)
